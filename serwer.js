3bf8078cd2960cccf5298ee1563a73ba5c5d4d72
Maciej <maciek.czekanski@op.pl>
2022-01-04 22:59:06 +0100

    commit po zmianie w pliku

--- serwer.js
+++ serwer.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000; 
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku!</h1>")
+    res.send("<h1>my app on heroku! - mała zmioana w pliku!</h1>")
 })
 
 app.listen(PORT, function () {
