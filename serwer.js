e41e4b486c703f562346d6998c0b16aa5b09d316
Maciej <maciek.czekanski@op.pl>
2022-01-04 23:48:18 +0100

    gg

--- serwer.js
+++ serwer.js
@@ -3,9 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 3000; 
 
 app.get("/", function (req, res) {
-    let form  = "<h1>Maciej Czekański 3i1</h1><form action ='/handleForm' method='GET'><input type='submit'>"
     
-    res.send(form)
+    
+    res.send("<h1>Maciej Czekański 3i1</h1><form action ='/handleForm' method='GET'><input type='submit'></form>")
 })
 
 app.listen(PORT, function () {
