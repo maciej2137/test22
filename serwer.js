6c96b6d5fc579f640f0f72c0a7d916955fc7c082
Maciej <maciek.czekanski@op.pl>
2022-01-04 23:51:41 +0100

    no i luksja

--- serwer.js
+++ serwer.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
     res.send("<h1>Maciej Czekański 3i1</h1><form action ='/handleForm' method='GET'><input type='submit'></form>")
 })
+app.get("/handleForm",function(req, res){
+    let obj={
+        imie:"Maciej",
+        nazwisko:"Czekański",
+        klasa:"3i1",
+        grupa:"Grupa 1"
+    }
+})
 
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
